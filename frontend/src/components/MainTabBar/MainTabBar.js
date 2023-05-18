a145a7cdc6984f80e2b957e4b660a98083c495ea
felipe16030 <felipebergerman@gmail.com>
2023-05-18 00:38:37 -0400

    MainTabBar
    Added MainTabBar component

--- frontend/src/components/MainTabBar/MainTabBar.js
+++ frontend/src/components/MainTabBar/MainTabBar.js
@@ -1,7 +1,60 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
-export default MainTabBar = () => {
+const MainTabBar = () => {
+
     return (
-        <Text> MainTabBar </Text>
-)}
+        <View style={styles.MainTabBarStyle}>
+            <TouchableOpacity>
+                <Image
+                    source={require('../../assets/Home.png')}
+                    style={styles.barImageLeft}
+                />
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Image
+                    source={require('../../assets/Plus.png')}
+                    style={styles.barImageMiddle}
+                />
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Image
+                    source={require('../../assets/User.png')}
+                    style={styles.barImageRight}
+                />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    barImageLeft: {
+        position: 'relative',
+        width: 30,
+        height: 30,
+        left: '70%'
+    },
+    barImageRight: {
+        position: 'relative',
+        width: 30,
+        height: 30,
+        right: '70%'
+    },
+    barImageMiddle: {
+        position: 'relative',
+        width: 30,
+        height: 30,
+    },
+    MainTabBarStyle: {
+        backgroundColor: 'white',
+        minWidth: '80%',
+        maxWidth: '80%',
+        minHeight: '8%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderRadius: '100%'
+    }
+});
+
+export default MainTabBar
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -2,8 +2,9 @@ import React, { useLayoutEffect } from 'react';
 import { View, StyleSheet, SafeAreaView, TouchableOpacity, Image } from 'react-native';
 import { IconButton, Button, Text } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
+import MainTabBar from '../../components/MainTabBar/MainTabBar';
 
-export default HomePage = () => {
+const HomePage = () => {
     const navigation = useNavigation();
 
     useLayoutEffect(() => {
@@ -13,25 +14,28 @@ export default HomePage = () => {
     }, []);
 
     return (
-        <SafeAreaView style={{ backgroundColor: '#071930',}}>
-            <View style={{ backgroundColor: '#071930', width: '100%', height: '100%', zIndex: '0',
-                alignItems: 'center'}}>
-                <View style={{zIndex: '1', flexDirection: 'row', display: 'flex', alignItems: 'center'}}>
+        <SafeAreaView style={{ backgroundColor: '#071930', flex: 1, alignItems: 'center' }}>
+            <View style={{ backgroundColor: '#071930', zIndex: '0', alignItems: 'center', flex: 3 }}>
+                <View style={{ zIndex: '1', flexDirection: 'row', display: 'flex', alignItems: 'center' }}>
                     <IconButton icon='cog' iconColor='#000000' backgroundColor='#FFFFFF' size={34} />
                     <TouchableOpacity style={{ marginHorizontal: '5%' }}>
-                        <View style={{ backgroundColor: '#FFFFFF', borderRadius: 20, alignItems: 'center', justifyContent: 'center', paddingHorizontal: '12%', paddingVertical: '6%'}}>
+                        <View style={{ backgroundColor: '#FFFFFF', borderRadius: 20, alignItems: 'center', justifyContent: 'center', paddingHorizontal: '12%', paddingVertical: '6%' }}>
                             <Text style={styles.text1}>Paris, France</Text>
                         </View>
                     </TouchableOpacity>
-                    <IconButton icon='email' iconColor='#000000' backgroundColor='#FFFFFF' size={34}/>
+                    <IconButton icon='email' iconColor='#000000' backgroundColor='#FFFFFF' size={34} />
                 </View>
-                <View style={{zIndex: '1'}}>
-
+                <View style={{ zIndex: '1' }}>
                 </View>
+            </View>
+            <View style={{ flex: 0.25, zIndex: '1' }}>
+                <MainTabBar />
             </View>
         </SafeAreaView>
     );
 }
+
+export default HomePage
 
 const styles = StyleSheet.create({
     base1: {
