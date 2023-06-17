de61526c1c0cb023e23768c2221ef280cfe01e59
benmadar <121907418+benmadar@users.noreply.github.com>
2023-06-17 10:26:13 -0400

    Added navigation to Home and Part of AccountPage

---
+++ frontend/src/pages/BecomeALocal/BecomeALocal.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const BecomeALocal = () => {
+  return (
+    <View>
+      <Text>BecomeALocal</Text>
+    </View>
+  )
+}
+
+export default BecomeALocal
+
+const styles = StyleSheet.create({
+    
+})
---
+++ frontend/src/pages/MyFriends/MyFriends.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MyFriends = () => {
+  return (
+    <View>
+      <Text>MyFriends</Text>
+    </View>
+  )
+}
+
+export default MyFriends
+
+const styles = StyleSheet.create({
+    
+})
