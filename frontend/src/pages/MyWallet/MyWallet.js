debee15a05b18144c43bbab745635d94576419c3
benmadar <121907418+benmadar@users.noreply.github.com>
2023-06-17 19:44:32 -0400

    Almost all of basic Navigation Done for now

---
+++ frontend/src/pages/MyWallet/MyWallet.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MyWallet = () => {
+  return (
+    <View>
+      <Text>MyWallet</Text>
+    </View>
+  )
+}
+
+export default MyWallet
+
+const styles = StyleSheet.create({
+    
+})
