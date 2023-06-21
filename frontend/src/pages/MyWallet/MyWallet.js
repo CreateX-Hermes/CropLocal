0f1a92352e1e5d62aff8116ceb9bdb850e3218c3
benmadar <121907418+benmadar@users.noreply.github.com>
2023-06-20 23:23:25 -0400

    Static Pages frontend for Help and My Wallet Pages

--- frontend/src/pages/MyWallet/MyWallet.js
+++ frontend/src/pages/MyWallet/MyWallet.js
@@ -1,10 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import { Divider} from 'react-native-paper';
 
 const MyWallet = () => {
   return (
-    <View>
-      <Text>MyWallet</Text>
+    <View style={{ backgroundColor: '#FFFFFF', height: '100%' }}>
+      <Text style={styles.text1}>
+          My Wallet
+      </Text>
+      <View style={{ backgroundColor: '#F5F5F5', height: '7%', borderRadius: 50, width: '86%', alignSelf: 'center', marginTop: '8%', flexDirection: 'row' }}>
+      <Image source={require('../../assets/visa.png')} style={ styles.text9 }/>
+        <Text style={styles.text6}>
+          7039 9323 832 239
+        </Text>
+        <Text style={styles.text8}>
+          09/24
+        </Text>
+      </View>
+      <View style={{ backgroundColor: '#F5F5F5', height: '7%', borderRadius: 50, width: '86%', alignSelf: 'center', marginTop: '8%', flexDirection: 'row' }}>
+      <Image source={require('../../assets/visa.png')} style={ styles.text9 }/>
+        <Text style={styles.text6}>
+          7039 9323 832 239
+        </Text>
+        <Text style={styles.text8}>
+          09/24
+        </Text>
+      </View>
+      <Divider style={{ width: '86%', alignSelf: 'center', marginTop: '10%', paddingTop: '0.4%', }}/>
+      <Text style={styles.text4}>
+          Payment Methods
+      </Text>
+      <View style={{ paddingTop: '6%', width: '35%', marginLeft: '7%' }}>
+                <TouchableOpacity 
+                style={{ 
+                    backgroundColor: '#288FBB',
+                    borderRadius: 50,
+                    paddingVertical: '8%'
+                }}>
+                        <Text style={styles.text7}>Edit Payment</Text>
+                </TouchableOpacity>
+                </View>
     </View>
   )
 }
@@ -12,5 +47,68 @@ const MyWallet = () => {
 export default MyWallet
 
 const styles = StyleSheet.create({
-    
+  text1: {
+    color: '#000000',
+    fontWeight: '600',
+    fontSize: '36em',
+    paddingTop: '10%',
+    paddingLeft: '7%'
+  }, 
+  text2: {
+    color: '#000000',
+    fontWeight: '400',
+    fontSize: '16em',
+    paddingTop: '6%',
+    paddingLeft: '7%',
+    paddingRight: '7%'
+  }, 
+  text3: {
+    color: '#000000',
+    fontWeight: '400',
+    fontSize: '16em',
+    paddingTop: '4%',
+    paddingLeft: '7%',
+    paddingRight: '7%'
+  }, 
+  text4: {
+    color: '#000000',
+    fontWeight: '600',
+    fontSize: '26em',
+    paddingTop: '8%',
+    paddingLeft: '7%'
+  }, 
+  text5: {
+    color: '#000000',
+    fontWeight: '600',
+    fontSize: '18em',
+    alignSelf: 'center',
+    paddingLeft: '7%'
+  },
+  text6: {
+    color: '#000000',
+    fontWeight: '600',
+    fontSize: '16em',
+    alignSelf: 'center',
+    paddingLeft: '8%'
+  }, 
+  text7: {
+    display: 'flex',
+    color: '#FFFFFF',
+    fontWeight: '600',
+    fontSize: '15em',
+    paddingHorizontal: '10%', alignSelf: 'center'
+  }, 
+  text8: {
+    color: '#C5C5C5',
+    fontWeight: '600',
+    fontSize: '16em',
+    alignSelf: 'center',
+    paddingLeft: '6%'
+  }, 
+  text9: {
+    width: 38,
+    height: 38,
+    alignSelf: 'center',
+    marginLeft: '7%'
+  },
 })
--- frontend/src/pages/Settings/Settings.js
+++ frontend/src/pages/Settings/Settings.js
@@ -1,16 +1,77 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import EditProfile from '../EditProfile/EditProfile.js';
+import BecomeALocal from '../BecomeALocal/BecomeALocal.js';
+import MyWallet from '../MyWallet/MyWallet.js';
+import Help from '../Help/Help.js';
 
 const Settings = () => {
+  const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            
+        });
+    }, []);
+
   return (
-    <View>
-      <Text>Settings</Text>
+    <View >
+    <View style={{ alignSelf: 'center', width: '86%', paddingTop: '6%'}}>
+          <TouchableOpacity style={{ backgroundColor: '#FFFFFF', alignItems: 'center', paddingVertical: '5%', borderRadius: 50, flexDirection: 'row', paddingHorizontal: '9%'}}
+          onPress={() => navigation.navigate(EditProfile)} >
+            <Image source={require('../../assets/profileUser.png')} style={ styles.text15 }/>
+            <Text style={styles.text9}>Edit Profile</Text>
+          </TouchableOpacity>
     </View>
+    <View style={{ alignSelf: 'center', width: '86%', paddingTop: '3%'}}>
+          <TouchableOpacity style={{ backgroundColor: '#FFFFFF', alignItems: 'center', paddingVertical: '5%', borderRadius: 50, flexDirection: 'row', paddingHorizontal: '9%'}}
+          onPress={() => navigation.navigate(BecomeALocal)}>
+            <Image source={require('../../assets/write.png')} style={ styles.text15 }/>
+            <Text style={styles.text9}>Become A Local</Text>
+          </TouchableOpacity>
+    </View>
+    <View style={{ alignSelf: 'center', width: '86%', paddingTop: '3%'}}>
+          <TouchableOpacity style={{ backgroundColor: '#FFFFFF', alignItems: 'center', paddingVertical: '5%', borderRadius: 50, flexDirection: 'row', paddingHorizontal: '9%'}}
+          onPress={() => navigation.navigate(MyWallet)}>
+            <Image source={require('../../assets/dollar.png')} style={ styles.text15 }/>
+            <Text style={styles.text9}>My Wallet</Text>
+          </TouchableOpacity>
+    </View>
+    <View style={{ alignSelf: 'center', width: '86%', paddingTop: '3%'}}>
+          <TouchableOpacity style={{ backgroundColor: '#FFFFFF', alignItems: 'center', paddingVertical: '5%', borderRadius: 50, flexDirection: 'row', paddingHorizontal: '9%'}}
+          onPress={() => navigation.navigate(Help)}>
+            <Image source={require('../../assets/communication.png')} style={ styles.text15 }/>
+            <Text style={styles.text9}>Help</Text>
+          </TouchableOpacity>
+    </View>
+</View>
   )
 }
 
 export default Settings
 
 const styles = StyleSheet.create({
-    
+  container: {
+    flex: 1,
+    borderRadius: 20, alignItems: 'center', alignSelf: 'center', paddingVertical: '2.8%'
+  },
+  text1: {
+      fontWeight: '700',
+      fontSize: '20em',
+      position: 'relative',
+      alignSelf: 'center',
+    },
+    text9: {
+      display: 'flex',
+      color: '#000000',
+      fontWeight: '600',
+      fontSize: '16em',
+      paddingLeft: '8%'
+    },
+    text15: {
+      width: 38,
+      height: 38,
+    },
 })
