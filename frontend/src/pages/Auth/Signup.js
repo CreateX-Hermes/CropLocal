0807af522ebc10f226a0e9388b47242502506feb
benmadar <121907418+benmadar@users.noreply.github.com>
2023-06-22 15:36:21 -0400

    Fixed login and signup pages, started home filter

--- frontend/src/pages/Auth/Signup.js
+++ frontend/src/pages/Auth/Signup.js
@@ -1,16 +1,163 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View, TextInput, SafeAreaView, TouchableOpacity} from 'react-native';
+import React, { useLayoutEffect, useState } from 'react'
+import {useNavigation} from '@react-navigation/native';
+import BottomTabNavigator from '../Navigators/BottomTabNavigator';
 
 const Signup = () => {
-  return (
-    <View>
-      <Text>Signup</Text>
-    </View>
-  )
-}
+  // const {navigation} = props;
+  const navigation = useNavigation();
 
-export default Signup
+  useLayoutEffect(() => {
+    navigation.setOptions({
+        headerShown: false,
+    });
+}, []);
+
+  return (
+    <SafeAreaView style={styles.main}>
+      <View style={styles.container}>
+        <View style={styles.wFull}>
+          <View style={styles.row}>
+            <Text style={styles.brandName}>Hermes</Text>
+          </View>
+
+          <Text style={styles.loginContinueTxt}>Sign Up</Text>
+          <TextInput style={styles.input} placeholder="Full Name" />
+          <TextInput style={styles.input} placeholder="Email" />
+          <TextInput style={styles.input} placeholder="Create a Password" />
+
+          <View style={styles.loginBtnWrapper}>
+              {/******************** SIGNUP BUTTON *********************/}
+              <TouchableOpacity
+                onPress={() => navigation.navigate(BottomTabNavigator)}
+                style={styles.loginBtn}>
+                <Text style={styles.loginText}>Sign Up</Text>
+              </TouchableOpacity>
+          </View>
+
+          {/***************** FORGOT PASSWORD BUTTON *****************/}
+          <TouchableOpacity
+          onPress={() => navigation.navigate(ForgotPassword)}>
+            <Text style={styles.forgotPassText}>Forgot Password?</Text>
+          </TouchableOpacity>
+
+
+        </View>
+
+        <View style={styles.footer}>
+          <Text style={styles.footerText}> Already have an account? </Text>
+          {/******************** Signup BUTTON *********************/}
+          <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          >
+            <Text style={styles.signupBtn}>Log In</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Signup;
 
 const styles = StyleSheet.create({
-    
-})
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '16%',
+  },
+  container: {
+    padding: '5%',
+    width: '100%',
+    position: 'relative',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  brandName: {
+    fontSize: '48em',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#E5B07A',
+    opacity: 0.9,
+  },
+  loginContinueTxt: {
+    fontSize: '20em',
+    textAlign: 'center',
+    color: '#071930',
+    marginBottom: '10%',
+    fontWeight: 600,
+    bottom: '6%'
+  },
+  input: {
+    borderWidth: 1.5,
+    borderColor: '#071930',
+    padding: '5%',
+    marginVertical: '3%',
+    borderRadius: '100%',
+    height: '10.6%',
+    fontSize: '16.5em',
+    fontWeight: '400',
+    paddingLeft: '7%'
+  },
+  // Login Btn Styles
+  loginBtnWrapper: {
+    height: '13%',
+    marginTop: '5%',
+    elevation: 5,
+  },
+  linearGradient: {
+    width: '100%',
+    borderRadius: 50,
+  },
+  loginBtn: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    width: '36%',
+    height: '62%',
+    backgroundColor: '#E5B07A',
+    borderRadius: 50
+  },
+  loginText: {
+    color: '#FFFFFF',
+    fontSize: '16em',
+    fontWeight: '500',
+  },
+  forgotPassText: {
+    color: '#071930',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: '3%',
+  },
+  // footer
+  footer: {
+    position: 'absolute',
+    bottom: '3%',
+    textAlign: 'center',
+    flexDirection: 'row',
+  },
+  footerText: {
+    color: '#071930',
+    fontWeight: 'bold',
+  },
+  signupBtn: {
+    color: '#E5B07A',
+    fontWeight: 'bold',
+  },
+  // utils
+  wFull: {
+    width: '100%',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: '10%',
+  },
+  mr7: {
+    marginRight: '7%',
+  },
+});
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -23,6 +23,13 @@ const HomePage = () => {
 
     const [isModalVisible, setModalVisible] = useState(false);
 
+    const [isFilterModalVisible, setFilterModalVisible] = useState(false);
+
+    const toggleFilterModal = () => {
+        setFilterModalVisible(!isFilterModalVisible);
+    };
+
+
     const toggleModal = () => {
         setModalVisible(!isModalVisible);
     };
@@ -62,6 +69,7 @@ const HomePage = () => {
     return (
         <SafeAreaView style={{ backgroundColor: '#071930', flex: 1, alignItems: 'center' }}>
             <View style={{ backgroundColor: '#071930', zIndex: '0', alignItems: 'center', flex: 3 }}>
+                {/*/ Actual Home Page Content */}
                 <View style={{ zIndex: '1', flexDirection: 'row', display: 'flex', alignItems: 'center' }}>
                     <IconButton 
                     onPress={toggleModal}
@@ -69,7 +77,7 @@ const HomePage = () => {
                     backgroundColor='#FFFFFF' 
                     size={34} />
                     <TouchableOpacity 
-                    onPress={() => navigation.navigate(HomePageFilter)}
+                    onPress={toggleFilterModal}
                     style={{ marginHorizontal: '5%' }}>
                         <View style={{ backgroundColor: '#FFFFFF', borderRadius: 20, alignItems: 'center', justifyContent: 'center', paddingHorizontal: '12%', paddingVertical: '6%' }}>
                             <Text style={styles.text1}>Paris, France</Text>
@@ -81,48 +89,69 @@ const HomePage = () => {
                     backgroundColor='#FFFFFF' 
                     size={34} />
                 </View>
+
+                {/*/ Settings Modal */}
                 <Modal visible={isModalVisible} animationType="fade" >
+                    <SafeAreaView  backgroundColor='#071930'>
                     <View backgroundColor='#071930' height='100%'>
                     <IconButton 
                         onPress={toggleModal}
                         icon='window-close' iconColor='#000000' 
                         backgroundColor='#FFFFFF' 
                         size={34}
-                        style={{ marginLeft: '6%', marginTop: '13.6%' }} />
+                        style={{ marginLeft: '6%'}} />
                         
                         <View style={{ alignSelf: 'center', width: '86%', paddingTop: '4%'}}>
-                            <TouchableOpacity style={{ backgroundColor: '#FFFFFF', alignItems: 'center', paddingVertical: '4.5%', borderRadius: 50, flexDirection: 'row', paddingHorizontal: '9%'}}
+                            <TouchableOpacity style={{ backgroundColor: '#FFFFFF', alignItems: 'center', paddingVertical: '4%', borderRadius: 50, flexDirection: 'row', paddingHorizontal: '9%'}}
                             onPress={handleEditProfilePress}  >
                                 <Image source={require('../../assets/profileUser.png')} style={ styles.text15 }/>
                                 <Text style={styles.text19}>Edit Profile</Text>
                             </TouchableOpacity>
                         </View>
                         <View style={{ alignSelf: 'center', width: '86%', paddingTop: '3%'}}>
-                            <TouchableOpacity style={{ backgroundColor: '#FFFFFF', alignItems: 'center', paddingVertical: '4.5%', borderRadius: 50, flexDirection: 'row', paddingHorizontal: '9%'}}
+                            <TouchableOpacity style={{ backgroundColor: '#FFFFFF', alignItems: 'center', paddingVertical: '4%', borderRadius: 50, flexDirection: 'row', paddingHorizontal: '9%'}}
                             onPress={handleBecomeALocalPress}>
                                 <Image source={require('../../assets/write.png')} style={ styles.text15 }/>
                                 <Text style={styles.text19}>Become A Local</Text>
                             </TouchableOpacity>
                         </View>
                         <View style={{ alignSelf: 'center', width: '86%', paddingTop: '3%'}}>
-                            <TouchableOpacity style={{ backgroundColor: '#FFFFFF', alignItems: 'center', paddingVertical: '4.5%', borderRadius: 50, flexDirection: 'row', paddingHorizontal: '9%'}}
+                            <TouchableOpacity style={{ backgroundColor: '#FFFFFF', alignItems: 'center', paddingVertical: '4%', borderRadius: 50, flexDirection: 'row', paddingHorizontal: '9%'}}
                             onPress={handleMyWalletPress}>
                                 <Image source={require('../../assets/dollar.png')} style={ styles.text15 }/>
                                 <Text style={styles.text19}>My Wallet</Text>
                             </TouchableOpacity>
                         </View>
                         <View style={{ alignSelf: 'center', width: '86%', paddingTop: '3%'}}>
-                            <TouchableOpacity style={{  backgroundColor: '#FFFFFF', alignItems: 'center', paddingVertical: '4.5%', borderRadius: 50, flexDirection: 'row', paddingHorizontal: '9%'}}
+                            <TouchableOpacity style={{  backgroundColor: '#FFFFFF', alignItems: 'center', paddingVertical: '4%', borderRadius: 50, flexDirection: 'row', paddingHorizontal: '9%'}}
                             onPress={handleHelpPress}>
                                 <Image source={require('../../assets/communication.png')} style={ styles.text15 }/>
                                 <Text style={styles.text19}>Help</Text>
                             </TouchableOpacity>
                         </View>
-                        <TouchableOpacity onPress={handleSignOutPress} style={{ backgroundColor: '#288FBB', width: '30%', borderRadius: 50, paddingVertical: '3.4%', marginTop: '12%', alignSelf: 'center', top: '30%' }}>
+                        <TouchableOpacity onPress={handleSignOutPress} style={{ backgroundColor: '#288FBB', width: '30%', borderRadius: 50, paddingVertical: '3.4%', marginTop: '12%', alignSelf: 'center', top: '36%' }}>
                             <Text style={ styles.text10}>Sign out</Text>
                         </TouchableOpacity>  
                     </View>
+                    </SafeAreaView>
                 </Modal>
+
+                {/*/ FilterModal */}
+                <Modal visible={isFilterModalVisible} animationType="fade">
+                    <SafeAreaView  backgroundColor='#FFFFFF'>
+                        <View backgroundColor='#FFFFFF' height='100%'>
+                            <IconButton 
+                            onPress={toggleFilterModal}
+                            icon='window-close' iconColor='#000000' 
+                            backgroundColor='#F5F5F5' 
+                            size={34}
+                            style={{ marginLeft: '6%'}} />
+
+                            <Text style={styles.text17}> Where are you traveling?</Text>
+                        </View>
+                    </SafeAreaView>
+                </Modal>
+
             </View>
         </SafeAreaView>
     );
@@ -151,13 +180,6 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         justifyContent: 'center',
       },
-          text19: {
-            display: 'flex',
-            color: '#000000',
-            fontWeight: '600',
-            fontSize: '16em',
-            paddingLeft: '8%'
-          },
           text15: {
             width: 38,
             height: 38,
@@ -169,4 +191,17 @@ const styles = StyleSheet.create({
             borderRadius: 100,
             padding: '4%'
           },
+          text17: {
+            fontWeight: '600',
+            fontSize: '26em',
+            paddingTop: '2%',
+            paddingLeft: '7%'
+          }, 
+          text19: {
+            display: 'flex',
+            color: '#000000',
+            fontWeight: '600',
+            fontSize: '16em',
+            paddingLeft: '8%'
+          }
 });
