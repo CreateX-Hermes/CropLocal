c1d8d6ca77a35debca42a11e5efaab0b408b045e
johannesq23 <johannesq23@gmail.com>
2023-11-01 22:45:35 -0400

    fix logout error

--- frontend/src/redux/reducers/userSlice.js
+++ frontend/src/redux/reducers/userSlice.js
@@ -36,6 +36,7 @@ export const userSlice = createSlice({
     builder.addCase(logoutUserAction.fulfilled, (state) => {
       state.isAuthenticated = false;
       state.user = {};
+      state.token = '';
     });
   },
 });
