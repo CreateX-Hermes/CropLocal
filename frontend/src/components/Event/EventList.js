e03ac35a42a975276d9479c421f18cd5f9cf7ca4
Logan Weekes <ltweekes1@gmail.com>
2023-11-07 20:26:07 -0500

    added header

--- frontend/src/components/Event/EventList.js
+++ frontend/src/components/Event/EventList.js
@@ -43,9 +43,9 @@ export function EventList() {
 const styles = StyleSheet.create({
   container: {
     width: '100%',
+    height: '100%',
     display: 'flex',
     alignItems: 'center',
-    marginTop: 60,
   },
   main: {
     height: '100%',
--- frontend/src/pages/EventPage/EventPage.js
+++ frontend/src/pages/EventPage/EventPage.js
@@ -1,9 +1,27 @@
-import { View } from "react-native";
+import { View, ScrollView, Image } from "react-native";
 
-export const EventPage = () => {
+export const EventPage = ({ route }) => {
+    const { data } = route.params;
+
     return (
         <View>
-
+            <ScrollView contentContainerStyle={styles.main}>
+            <Image
+                source={data.image}
+                defaultSource={require('../../assets/market_stand.png')}
+                style={{ width: 400, height: 250 }}
+            />
+            </ScrollView>
         </View>
     )
+}
+
+const styles = {
+    main: {
+        display: 'flex',
+        position: 'fixed',
+        alignItems: 'center',
+        flexDirection: 'column',
+        paddingBottom: '100%',
+      },
 }
